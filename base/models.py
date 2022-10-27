e5d50209b5815ec03f2839e936f87b672b36595a
bishadcs <68679199+binshadcs@users.noreply.github.com>
2022-10-27 16:34:10 +0530

    model added

--- base/models.py
+++ base/models.py
@@ -1,7 +1,9 @@
+from email.policy import default
 from pyexpat import model
 from statistics import mode
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Department(models.Model):
@@ -30,4 +32,56 @@ class Time_set(models.Model):
     exclusion = models.DateTimeField()
 
 
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN" , 'Admin'
+        STAFF = "STAFF" , 'Staff'
+        CONVENOR = "CONVENOR" , 'Convenor'
+        CHAIRMAN = "CHAIRMAN" , 'Chairman'
+        DIRECTOR = "DIRECTOR", 'Director'
+    role = models.CharField(max_length=50, choices=Role.choices)
+    dep = models.ForeignKey(Department, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.username
+
+
+class work_schedule(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "pending"),
+        ("approved", "approved"),
+        ("rejected", "rejected")
+    )
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    start_td = models.DateTimeField()
+    end_td = models.DateTimeField()
+    admin_status = models.CharField(max_length=50,choices = STATUS_CHOICES,default="pending")
+
+class meeting(models.Model):
+    PRIORITY = (
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5")
+    )
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    start_td = models.DateTimeField()
+    end_td = models.DateTimeField()
+    agenda = models.TextField()
+    vunue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    priority = models.PositiveSmallIntegerField(choices=PRIORITY)
+    minutes = models.TextField()
+
+class Participants(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "pending"),
+        ("approved", "approved"),
+        ("rejected", "rejected")
+    )
+    meeting_id = models.ForeignKey(meeting, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    responses = models.CharField(max_length=50,choices=STATUS_CHOICES,default="pending")
