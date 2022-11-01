202775721574ce09bdf4110f443c89bb153cdf47
Jishna-tp <84098652+Jishna-tp@users.noreply.github.com>
2022-11-01 18:28:51 +0530

    Add files via upload

--- base/admin.py
+++ base/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from base.models import Department,Freezing_time,Venue,Time_set, User
+from base.models import Department,Freezing_time,Venue,Time_set, User,meeting, work_schedule, Participants
 
 admin.site.register(Department)
 admin.site.register(Freezing_time)
 admin.site.register(Venue)
 admin.site.register(Time_set)
 admin.site.register(User)
+admin.site.register(meeting)
+admin.site.register(work_schedule)
+admin.site.register(Participants)
 
 # Register your models here.
--- base/views.py
+++ base/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('users')
+
+@login_required
+def sample(request):
+    return render(request, 'base/admin.html')
+
+def users(request):
+    return render(request, 'base/admin.html')
+
+def settings(request):
+    return render(request, 'base/settings.html')
+def profile(request):
+    return render(request, 'base/profile.html')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('users')
